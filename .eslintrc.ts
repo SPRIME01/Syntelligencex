2f4d845c159ffe89a7fe5383c491a2611a4d9fa4
Samuel Prime <sprime01@gmail.com>
2024-09-09 16:31:30 -0400

    Added Scons.

---
+++ .eslintrc.ts
@@ -0,0 +1,13 @@
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  newProperty: 'newValue',
+};
